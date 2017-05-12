730cff32fc73ffe7254528bba3c0d0147fc5b4e5
Colin <colin.bernet@cern.ch>
2017-05-12 11:51:45 +0200

    compfiles: generic way to find sample root files

--- components/ZH_Zmumu.py
+++ components/ZH_Zmumu.py
@@ -1,30 +1,31 @@
 import heppy.framework.config as cfg
 import heppy.utils.absglob as absglob
+import compfiles
 
-basedir = '/Users/cbernet/Code/FCC/fcc-ee-higgs/samples/pythia'
+basedir = '/Users/cbernet/Code/FCC/fcc_ee_higgs/samples/pythia'
 
 # definition of input samples 
-ZH_Zmumu = cfg.MCComponent(
-    'ZH_Zmumu',
-    files = absglob.glob('{}/mumu/ZH/ee_ZH_Zmumu_*.root'.format(basedir)),
+ZH = cfg.MCComponent(
+    'ZH',
+    files = compfiles.get('{}/ZH'.format(basedir)),
     xSection = 0.00653, # pb-1
     nGenEvents = 4e4)
 
 ZZ = cfg.MCComponent(
     'ZZ',
-    files = absglob.glob('{}/ZZ/ee_ZZ_*.root'.format(basedir)),
+    files = compfiles.get('{}/ZZ'.format(basedir)),
     xSection = 1.360, # pb-1
     nGenEvents = 4e4)
 
 WW = cfg.MCComponent(
     'WW',
-    files = absglob.glob('{}/WW/ee_WW_*.root'.format(basedir)),
-    # files = absglob.glob('WW/Job_*/ee_WW.root'),
+    files = compfiles.get('{}/WW'.format(basedir)),
+    # files = compfiles.get ('WW/Job_*/ee_WW.root'),
     xSection = 16.330, # pb-1
     nGenEvents = 4e4)
 
 components = dict(
-        ( (comp.name, comp) for comp in [ZH_Zmumu, ZZ, WW] )
+        ( (comp.name, comp) for comp in [ZH, ZZ, WW] )
 )
 
 if __name__ == '__main__':
---
+++ components/compfiles.py
@@ -0,0 +1,33 @@
+'''Find all rootfiles for a given sample.
+'''
+
+import os 
+from heppy.utils import absglob as glob
+
+def get(dirname):
+    '''Returns the list of absolute paths for all root files in the sample.
+     
+    The sample must be on a standard filesystem,
+    and the root files must be organized in the following way:
+    
+    dirname/*_*.root
+
+    or:
+
+    dirname/Job_*/*.root
+    '''
+    if not os.path.isdir(dirname):
+        raise ValueError('{} is not a directory'.format(dirname))
+    rootfiles = glob.glob('{}/*_*.root'.format(dirname))
+    if len(rootfiles):
+        return rootfiles
+    rootfiles = glob.glob('{}/Job*/*.root'.format(dirname)) 
+    if len(rootfiles):
+        return rootfiles
+    raise ValueError('no rootfile found in component ' + dirname)
+
+if __name__ == '__main__':
+    import sys
+    import pprint
+    pprint.pprint(get(sys.argv[1]))
+    
