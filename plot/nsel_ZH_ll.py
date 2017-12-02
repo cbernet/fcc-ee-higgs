fce54b539cad80da4b7ad36e14472d93b1b920eb
Colin <colin.bernet@cern.ch>
2017-12-02 09:52:43 +0100

    improved printout for nsel macro

--- plot/nsel_ZH_ll.py
+++ plot/nsel_ZH_ll.py
@@ -5,8 +5,14 @@ from fcc_ee_higgs.plot.plot_ZH_ll import cut_Z
 
 if __name__ == '__main__':
     
-    rfile = TFile(sys.argv[1])
+    dataset = sys.argv[1]
+    rfname = '/'.join([dataset, 'fcc_ee_higgs.analyzers.ZHTreeProducer.ZHTreeProducer_1/tree.root'])
+    sfname = '/'.join([dataset, 'software.yaml'])
+    rfile = TFile(rfname)
     tree = rfile.Get('events')
-    tree.Draw('zeds_m', cut_Z)
+    tree.Draw('zeds_m', cut_Z, 'goff')
+    print sys.argv[1]
     print tree.GetSelectedRows()
+    with open(sfname) as soft:
+        print soft.read()
 
