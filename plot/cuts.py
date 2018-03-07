d35e2cc174890c890ff15458e94858735629a5e3
Colin <colin.bernet@cern.ch>
2018-03-07 08:16:07 +0100

    Cuts class based on OrderedDict

---
+++ plot/cuts.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+
+class Cuts(OrderedDict):
+    
+    def __init__(self, items):
+        super(Cuts, self).__init__(items)
+    
+    def __str__(self):
+        return ' && '.join(self.values())
+   
