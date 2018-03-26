76cd00256a17d830a7d1d23fa9dab1c5dc37fda1
Colin <colin.bernet@cern.ch>
2018-03-26 17:49:42 +0200

    added hzz contamination

--- plot/cuts_gen.py
+++ plot/cuts_gen.py
@@ -1,5 +1,6 @@
 cut_gen_htautau = 'abs(genboson2_1_pdgid)==15'
 cut_gen_hww = 'abs(genboson2_1_pdgid)==24'
+cut_gen_hzz = 'abs(genboson2_1_pdgid)==23'
 cut_gen_hbb = 'abs(genboson2_1_pdgid)==5'
 
 from fcc_ee_higgs.plot.cuts import Cuts
@@ -7,6 +8,7 @@ cuts_gen = Cuts([
     ('cut_gen_htautau', cut_gen_htautau), 
     ('cut_gen_hbb', cut_gen_hbb), 
     ('cut_gen_hww', cut_gen_hww), 
+    ('cut_gen_hzz', cut_gen_hzz), 
 ])
 
 def signal_contamination(tree, cut):
