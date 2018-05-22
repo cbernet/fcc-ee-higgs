a2b64e5c8621ab23f738ab5b5de661c0a3356a14
Colin <colin.bernet@cern.ch>
2018-05-22 15:39:50 +0200

    started to work on cut optimizer

---
+++ plot/cut_optimizer.py
@@ -0,0 +1,10 @@
+
+class CutOptimizer(object):
+    
+    def __init__(self, components, basecut, cuts):
+        self.components = components
+        self.basecut = basecut
+        self.cuts = cuts
+        
+    
+        
---
+++ plot/debug_cut_optimizer.py
@@ -0,0 +1,5 @@
+
+from plotconfig_ZH_ll import *
+from cut_optimizer import CutOptimizer
+
+cutopt = CutOptimizer(comps, basecut='1', cuts=cuts)
