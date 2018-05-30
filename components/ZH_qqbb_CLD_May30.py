056ab3f6f9425d350719a6266d9fd9ab770b01ec
Colin <colin.bernet@cern.ch>
2018-05-30 11:15:02 +0200

    qqbb analysis working - 0.46%

---
+++ components/ZH_qqbb_CLD_May30.py
@@ -0,0 +1,32 @@
+import os 
+from fcc_datasets.fcc_component import FCCComponent
+# import fcc_datasets.basedir as basedir
+# basedir.basename = '/Users/cbernet/Datasets/FCC/fcc_ee_higgs/samples'
+
+ZH = FCCComponent(
+    'heppy/ee_to_ZH_to_qqbb/May30/ee_to_ZH_Oct30',
+    xSection=2.e-10,
+    nGenEvents=5000*93,
+    uncertainty=1
+)
+
+ZZ = FCCComponent(
+    'heppy/ee_to_ZH_to_qqbb/May30/ee_to_ZZ_Sep12_A_2',
+    xSection=1.35e-9,
+    nGenEvents=10000*178.,
+    uncertainty=1
+)
+
+WW = FCCComponent(
+    'heppy/ee_to_ZH_to_qqbb/May30/ee_to_WW_Dec6_large',
+    xSection=1.64e-8,  
+    nGenEvents=20000*181.,
+    uncertainty=1    
+)
+
+ffbar = FCCComponent(
+    'heppy/ee_to_ZH_to_qqbb/May30/ee_to_ffbar_Sep12_B_4',
+    xSection=7.9e-8,
+    nGenEvents=10000*424.,
+    uncertainty=1
+)
--- plot/plotconfig_ZH_qqbb.py
+++ plot/plotconfig_ZH_qqbb.py
@@ -1,3 +1,23 @@
+from fcc_ee_higgs.plot.plotconfig_ZH_ll import cut_hbb, bins, detector, lumi, xtitle
+
+var = 'higgsmass'
+##var = 'mZedJet'
+##var = 'deltaWW'
+##var = 'vismass'
+
+channel = 'qqbb'
+
+from fcc_ee_higgs.components.ZH_qqbb_CLD_May30 import ZH, ZZ, ffbar, WW
+from fcc_ee_higgs.components.tools import load
+comps = [ZZ, ZH, ffbar, WW]
+ZH.name =  'ZH' 
+ZZ.name =  'ZZ'
+ffbar.name =  'ffbar'
+WW.name = 'WW'
+load(comps)
+
+from fcc_ee_higgs.plot.cuts import Cuts
+
 cut = "n_iso_leptons<=2 && hadjet1_n_constituents >= 5 && \
 hadjet1_n_charged_hadrons>0 && hadjet2_n_constituents >= 5 && \
 hadjet2_n_charged_hadrons>0 && hadjet3_n_constituents >= 5 && \
@@ -7,3 +27,49 @@ deltaWW>10 && deltaZZ > 10 &&  (hadjet1_higgsmaker*hadjet1_b + \
 hadjet2_higgsmaker*hadjet2_b + hadjet3_higgsmaker*hadjet3_b + \
 hadjet4_higgsmaker*hadjet4_b)==2 && mHJet > 100 && mZedJet > 80 && \
 mZedJet < 110"
+
+# no_lep = '(n_iso_leptons<=2)'
+no_lep = '(n_iso_leptons==0)'
+njets = '(n_jets>=4)'
+jet_id = '(hadjet1_n_constituents >= 5 && \
+hadjet1_n_charged_hadrons>0 && hadjet2_n_constituents >= 5 && \
+hadjet2_n_charged_hadrons>0 && hadjet3_n_constituents >= 5 && \
+hadjet3_n_charged_hadrons>0 && hadjet4_n_constituents >= 5)'
+vismass = '(vismass>=180)'
+chi2 = '(chi2>=0)'
+deltaWW = '(deltaWW>10)'
+deltaZZ = '(deltaZZ>10)'
+btag = '((hadjet1_higgsmaker*hadjet1_b + \
+hadjet2_higgsmaker*hadjet2_b + hadjet3_higgsmaker*hadjet3_b + \
+hadjet4_higgsmaker*hadjet4_b)==2)'
+mZ = '(mZedJet > 80 && mZedJet < 110)'
+# mZ = '(mZedJet > 82 && mZedJet < 102)'
+# mZ = '(mZedJet > 85 && mZedJet < 115)'
+mH = '(mHJet > 100)'
+
+cuts = Cuts([
+    ('no_lep', no_lep),
+    ('njets', njets), 
+    ('jet_id', jet_id), 
+    ('vismass', vismass), 
+    ('chi2', chi2), 
+    ('deltaWW', deltaWW), 
+    ('deltaZZ', deltaZZ), 
+    ('btag', btag), 
+    ('mZ', mZ), 
+    # ('mH', mH), 
+    ])
+
+if var == 'mZedJet':
+    del cuts['mZ']
+elif var == 'deltaWW':
+    del cuts['deltaWW']
+    bins = 50, 0, 100
+elif var == 'deltaZZ':
+    del cuts['deltaZZ']
+    bins = 50, 0, 100
+elif var == 'vismass':
+    del cuts['vismass']
+    bins = 50, 0, 250
+
+cut = str(cuts)
