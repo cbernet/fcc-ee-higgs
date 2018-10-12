fa957ee361c14a9f07b1a04c82f2f6c240ec8f48
Colin Bernet <colin.bernet@cern.ch>
2018-10-12 10:46:15 +0200

    tmva test script

---
+++ plot/mva/test_tmva.py
@@ -0,0 +1,95 @@
+
+import ROOT
+
+# create a TNtuple
+ntuple_sig = ROOT.TNtuple("ntuple_sig","ntuple_sig","x:y")
+ntuple_bgd = ROOT.TNtuple("ntuple_bgd","ntuple_bgd","x:y")
+
+# generate 'signal' and 'background' distributions
+for i in range(10000):
+    # throw a signal event centered at (1,1)
+    ntuple_sig.Fill(ROOT.gRandom.Gaus(1,1), # x
+                    ROOT.gRandom.Gaus(1,1) )
+    # throw a background event centered at (-1,-1)
+    ntuple_bgd.Fill(ROOT.gRandom.Gaus(-1,1), # x
+                    ROOT.gRandom.Gaus(-1,1) )
+
+# keeps objects otherwise removed by garbage collected in a list
+gcSaver = []
+
+# create a new TCanvas
+gcSaver.append(ROOT.TCanvas())
+
+# draw an empty 2D histogram for the axes
+histo = ROOT.TH2F("histo","",1,-5,5,1,-5,5)
+histo.Draw()
+
+# draw the signal events in red
+ntuple_sig.SetMarkerColor(ROOT.kRed)
+ntuple_sig.Draw("y:x","","same")
+
+# draw the background events in blue
+ntuple_bgd.SetMarkerColor(ROOT.kBlue)
+ntuple_bgd.Draw("y:x","","same")
+
+ROOT.TMVA.Tools.Instance()
+
+# note that it seems to be mandatory to have an
+# output file, just passing None to TMVA::Factory(..)
+# does not work. Make sure you don't overwrite an
+# existing file.
+fout = ROOT.TFile("test.root","RECREATE")
+
+factory = ROOT.TMVA.Factory("TMVAClassification", fout,
+                            ":".join([
+                                "!V",
+                                "!Silent",
+                                "Color",
+                                "DrawProgressBar",
+                                "Transformations=I;D;P;G,D",
+                                "AnalysisType=Classification"]
+                                     ))
+
+dataloader = ROOT.TMVA.DataLoader("dataset")
+dataloader.AddVariable("x","F")
+dataloader.AddVariable("y","F")
+signalWeight = 1.
+backgroundWeight = 1.
+dataloader.AddSignalTree    ( ntuple_sig,     signalWeight     )
+dataloader.AddBackgroundTree( ntuple_bgd, backgroundWeight )
+
+mycutSig = ROOT.TCut('1')
+mycutBkg = ROOT.TCut('1')
+dataloader.PrepareTrainingAndTestTree( mycutSig, mycutBkg,
+                                       "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" )
+
+factory.BookMethod( dataloader, ROOT.TMVA.Types.kBDT, "BDTG",
+                    "!H:!V:NTrees=1000:MinNodeSize=1.5%:BoostType=Grad:Shrinkage=0.10:UseBaggedBoost:BaggedSampleFraction=0.5:nCuts=20:MaxDepth=2" )        
+
+
+# Train MVAs
+factory.TrainAllMethods()
+
+# Test MVAs
+factory.TestAllMethods()
+
+# Evaluate MVAs
+factory.EvaluateAllMethods()    
+
+##
+##factory.AddSignalTree(ntuple)
+##factory.AddBackgroundTree(ntuple)
+##
+### cuts defining the signal and background sample
+##sigCut = ROOT.TCut("signal > 0.5")
+##bgCut = ROOT.TCut("signal <= 0.5")
+##
+##factory.PrepareTrainingAndTestTree(sigCut,   # signal events
+##                                   bgCut,    # background events
+##                                   ":".join([
+##                                       "nTrain_Signal=0",
+##                                        "nTrain_Background=0",
+##                                        "SplitMode=Random",
+##                                        "NormMode=NumEvents",
+##                                        "!V"
+##                                   ]))
